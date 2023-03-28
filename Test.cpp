ce2d670859b27fcec36354b6b3e1cbd819c39653
vboxuser <yeelacitron@gmail.com>
2023-03-28 21:51:38 +0300

    final

--- Test.cpp
+++ Test.cpp
@@ -6,6 +6,8 @@
 #include "sources/player.hpp"
 #include "sources/game.hpp"
 #include "sources/card.hpp"
+#define TURNS 5
+#define STACK_SIZE 52
 
 using namespace std;
 using namespace ariel;
@@ -20,19 +22,19 @@ TEST_CASE("The start of the game when no turns have been played"){
         CHECK(p2.cardesTaken()==0);
     }
     SUBCASE("Testing stacksize()"){
-            CHECK(p1.stacksize()==26);
-            CHECK(p2.stacksize()==26);
+            CHECK(p1.stacksize()==(STACK_SIZE/2));
+            CHECK(p2.stacksize()==(STACK_SIZE/2));
     }
-    SUBCASE("Throw exception in printWiner() when there is no winner at the start"){
+    SUBCASE("Throw exception in printWiner() when there is no winner at the beginning of the game"){
             CHECK_THROWS(game.printWiner());
     }
-    SUBCASE("shoudn't throw excepiton in print functions except printWiner()"){
+    SUBCASE("Shoudn't throw exception in print functions except for the function printWiner()"){
             CHECK_NOTHROW(game.printLastTurn());
             CHECK_NOTHROW(game.printLog());
             CHECK_NOTHROW(game.printStats());
     } 
 }
-TEST_CASE("Throw exception if the game has the same player"){
+TEST_CASE("Throw exception if both players are the same"){
     Player p1("bob");
     CHECK_THROWS_AS(Game game(p1,p1),invalid_argument);
 }
@@ -41,45 +43,53 @@ TEST_CASE("The game is not over and five turns have been played"){
     Player p1("bob");
     Player p2("alice");
     Game game(p1,p2); 
-    for (int i=0;i<5;i++) {
+    for (int i=0;i< TURNS;i++) {
         game.playTurn();
         if(p1.stacksize()==0){
             break;
         }
     }
-    REQUIRE(p1.stacksize() >0);
-    SUBCASE("Test that at leasts one of the players recieves cards and the right amount (a multiple of 2) in cardesTaken()"){
+    REQUIRE(p1.stacksize() > 0);
+    SUBCASE("Test that at least one of the players receives the right amount (a multiple of 2) of cards in cardesTaken()"){
             CHECK((p1.cardesTaken()>0 || p2.cardesTaken()>0));
             CHECK((p1.cardesTaken()%2==0 && p2.cardesTaken()%2==0));
-            CHECK((p1.cardesTaken()+ p2.cardesTaken())<=50);
-            CHECK((p1.cardesTaken()+ p2.cardesTaken())>=10);
+            CHECK((p1.cardesTaken()+ p2.cardesTaken())<=(STACK_SIZE -2));
+            CHECK((p1.cardesTaken()+ p2.cardesTaken())>=(TURNS*2));
     }
-    SUBCASE("Test that at least five card have been taken from the stacksize() of each player"){
-            CHECK(p1.stacksize() <=21);
-            CHECK(p2.stacksize() <=21);
+    SUBCASE("Test that at least five cards have been taken from the stacksize() of each player"){
+            CHECK(p1.stacksize() <=(STACK_SIZE/2-TURNS));
+            CHECK(p2.stacksize() <=(STACK_SIZE/2-TURNS));
     }
-    SUBCASE("Throw exption in printWiner() when there no winner because the game in not over"){
+    SUBCASE("Throw exception in printWiner() when the game in not over"){
             CHECK_THROWS(game.printWiner());
     }  
 }
 
-TEST_CASE("The game have been play and is over"){
+TEST_CASE("The game has been played and is over"){
     Player p1("bob");
     Player p2("alice");
     Game game(p1,p2); 
     game.playAll();
-    SUBCASE("Test that there no cards in stacksize()"){
+    SUBCASE("Test that there are no cards in stacksize()"){
             CHECK(p1.stacksize()==0);
             CHECK(p2.stacksize()==0);
     }
-    SUBCASE("Test that at least one of the player has cardesTaken()"){
+    SUBCASE("Test that at least one of the players has cardesTaken()"){
         CHECK((p1.cardesTaken()>0 || p2.cardesTaken()>0));
     }
-    SUBCASE("Throw exeption when trying to play turn after the end of the game"){
-        CHECK_THROWS(game.playTurn());
-        CHECK_THROWS(game.playAll());
+    int points_p1 = p1.cardesTaken();
+    int points_p2= p2.cardesTaken();
+    SUBCASE("Test the trying to play a trun (using playTurn()) after the end of the game doesn't change the previous outcome"){
+        game.playTurn();
+        CHECK(p1.cardesTaken()== points_p1);
+        CHECK(p2.cardesTaken()== points_p2);
     }
-    SUBCASE("The game is over and shoudn't throw exepiton in print functions"){
+    SUBCASE("Test the trying to play a trun (using playAll()) after the end of the game doesn't change the previous outcome"){
+        game.playAll();
+        CHECK(p1.cardesTaken()== points_p1);
+        CHECK(p2.cardesTaken()== points_p2);
+    }
+    SUBCASE("The game is over and shoudn't throw exceptions in the print functions"){
             CHECK_NOTHROW(game.printWiner());
             CHECK_NOTHROW(game.printLastTurn());
             CHECK_NOTHROW(game.printLog());
@@ -87,21 +97,22 @@ TEST_CASE("The game have been play and is over"){
     }  
 }
 
-TEST_CASE("The same player try to enter two differnt game"){
+TEST_CASE("The same player tried to play in two different games"){
     Player p1("bob");
     Player p2("alice");
     Player p3("alex");
     Game game1(p1,p2); 
     
-    SUBCASE("Throws exeption when trying to put Player in the second game"){
-            CHECK_THROWS_AS(Game game2(p1,p3),exception);
+    SUBCASE("Throws exception if Player p1 tries to join the second game"){
+            CHECK_THROWS_AS(Game game2(p1,p3),invalid_argument);
     }
     
-    SUBCASE("Shouldn't throw exception when trying to play firstgame"){
+    SUBCASE("Shouldn't throw exception when trying to play the first game"){
         CHECK_NOTHROW(game1.playTurn());
+        CHECK_NOTHROW(game1.playAll());
     }
     Player p4("ari");
-       SUBCASE("Shouldn't throw exception when p3 trying to join a differnt game"){
+    SUBCASE("Shouldn't throw exception when player p3 tries to join a different game"){
         CHECK_NOTHROW(Game game3(p3,p4));
     }
 
--- sources/player.cpp
+++ sources/player.cpp
@@ -9,7 +9,7 @@
     using namespace ariel;
     using namespace std;
     int Player:: stacksize(){
-        return 0;
+        return 52/2;
     }
     int Player:: cardesTaken(){
         return 5;
